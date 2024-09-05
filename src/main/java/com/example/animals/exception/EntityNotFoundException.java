e79208750a07e1554ee36e79e05698e7008af8f1
mkkubinsk <mk.kubinski@gmail.com>
2024-09-05 15:18:36 +0200

    Added liquibase

--- src/main/java/com/example/animals/exception/EntityNotFoundException.java
+++ src/main/java/com/example/animals/exception/EntityNotFoundException.java
@@ -1,7 +1,6 @@
 package com.example.animals.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
